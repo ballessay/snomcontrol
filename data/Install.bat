3dd0afebde7d4e4b98b4685fd448383dc0ab9115
ballessay <code@ballessay.de>
2019-07-06 22:32:56 +0200

    Modify Install.bat

--- data/Install.bat
+++ data/Install.bat
@@ -2,7 +2,7 @@ set TARGET_DIR=%ProgramFiles%\snomcontrol
 
 rmdir /Q /S "%TARGET_DIR%"
 
-xcopy /S /I /Y /Q "%~dp0\..\snomcontrol" "%TARGET_DIR%" /EXCLUDE:%~dp0\excludes.txt
+xcopy /S /I /Y /Q "%~dp0\bin" "%TARGET_DIR%" /EXCLUDE:%~dp0\excludes.txt
 
 reg import "%~dp0\snomcontrol.reg"
 pause
