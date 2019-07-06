d1cbdf8f49e4380217f8d74ea65d4e83fe34e425
ballessay <code@ballessay.de>
2019-07-06 19:30:38 +0200

    Fix install targets

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -rf build
 mkdir build
 cd build
 
@@ -7,12 +8,12 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr ..
 make -j
 
-make DESTDIR=appdir install
+DESTDIR=install make install
 
-wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage" && chmod a+x linuxdeployqt*.AppImage
+#wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage" && chmod a+x linuxdeployqt*.AppImage
 
-./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -no-strip
-./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -appimage -no-strip
+#./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -no-strip
+#./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -appimage -no-strip
 
 #find ./appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
 #ls -la
