2dbe636754a56f27c872b5e06e53fd10bf9037e7
ballessay <code@ballessay.de>
2019-07-08 10:55:47 +0200

    Fix AppImage SingleApplication behaviour
    Because AppImages get mounted to different folders in /tmp ignore the
    appliction dir.

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,9 @@
 
 int main(int argc, char *argv[])
 {
-    SingleApplication app(argc, argv, true);
+    SingleApplication::Options options = SingleApplication::User |
+                                         SingleApplication::ExcludeAppPath;
+    SingleApplication app(argc, argv, true, options);
 
     app.setOrganizationName("ballessay");
     app.setOrganizationDomain("ballessay.de");
